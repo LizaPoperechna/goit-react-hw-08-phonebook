6c4e0df5ffe2ea23961ba341cb6e6c9f4b182f4a
LizaPoperechna <107076245+LizaPoperechna@users.noreply.github.com>
2023-02-15 08:37:45 +0000

    Deploying to gh-pages from @ LizaPoperechna/goit-react-hw-08-phonebook@81e9ddb0321aa7eef1234aeb4291cfbc54d1b916 🚀

--- static/js/567.28d570ec.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[567],{567:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var i=n(184),r={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};function c(){return(0,i.jsx)("div",{style:r.container,children:(0,i.jsxs)("h1",{style:r.title,children:["Task manager welcome page"," "]})})}}}]);
-//# sourceMappingURL=567.28d570ec.chunk.js.map
---
+++ static/js/843.6d005174.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[843],{7843:function(e,n,t){t.r(n),t.d(n,{default:function(){return p}});var r,a=t(168),o=t(7691).ZP.h1(r||(r=(0,a.Z)(["\n  font-size: 32px;\n  text-align: center;\n  padding-left: 12px;\n  padding-right: 12px;\n\n"]))),c=t(184);function p(){return(0,c.jsx)(c.Fragment,{children:(0,c.jsx)(o,{children:"Welcome to Phonebook app"})})}}}]);
+//# sourceMappingURL=843.6d005174.chunk.js.map
